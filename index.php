e9e281f639d6b9eeef528ff78f5dfe471c961aae
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-10-05 13:02:05 +0300

    add <div> top

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DELFI - Žinios</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+      <div class="row">
+        <div class="col top">
+          <div class="row">
+            <div class="col"></div>
+            <a class="col-1" href="#">Internetas</a>
+            <a class="col-1" href="#">El.paštas</a>
+            <a class="col-1" href="#">Reklama</a>
+            <a class="col-1" href="#">Pagalba</a>
+            <a class="col-1" href="#">Delfi.lv</a>
+            <a class="col-1" href="#">Delfi.ee</a>
+          </div>
+        </div>
+      </div>
+
+
+
+  </div>
+</body>
+</html>
