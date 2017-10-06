e9aff169ed957118e378efa0d34854fc12ff36eb
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-10-06 10:49:39 +0300

    naujas

--- index.php
+++ index.php
@@ -6,42 +6,134 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
+  <script>
+    /* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+  </script>
+
   <div class="container">
+    <span class="menu navbar-toggler-icon" onclick="openNav()">open</span>
       <div class="row">
         <div class="col top">
           <div class="row">
             <div class="col"></div>
-            <a class="col-1" href="#">Internetas</a>
-            <a class="col-1" href="#">El.paštas</a>
-            <a class="col-1" href="#">Reklama</a>
-            <a class="col-1" href="#">Pagalba</a>
-            <a class="col-1" href="#">Delfi.lv</a>
-            <a class="col-1" href="#">Delfi.ee</a>
+            <a class="col-xl-1 col-12" href="#">Internetas</a>
+            <a class="col-xl-1 col-12" href="#">El.paštas</a>
+            <a class="col-xl-1 col-12" href="#">Reklama</a>
+            <a class="col-xl-1 col-12" href="#">Pagalba</a>
+            <a class="col-xl-1 col-12" href="#">Delfi.lv</a>
+            <a class="col-xl-1 col-12" href="#">Delfi.ee</a>
           </div>
         </div>
       </div>
       <div class="row">
-        <img  class="col-2 logo" src="images/logo.gif" alt="Delfi logo"></img>
+        
+        <img  class="col-xl-2 col-auto  logo" src="images/logo.gif" alt="Delfi logo"></img>
+        
       </div>
       <div class="row">
         <div class="col head">
           <div class="row">
-            <div class="col-6">
-              <div class="row">
-              <div class="col-5">vasario 20d., ketvirtadienis</div>
-              <a  class="col-2 btnZ">ŽINIOS</a>
-              <a  class="col-2 btnW">WEB A-Z</a>
-              <a  class="col-3 btnM">MANO DELFI</a>
+            <div class="col-xl-6 col-12 ">
+              <div class="row justify-col-content-center">
+              <div class="col-xl-5 col-12">vasario 20d., ketvirtadienis</div>
+              <a href="#" class="col-xl-2 col-12 btnZ">ŽINIOS</a>
+              <a href="#" class="col-xl-2 col-12 btnW">WEB A-Z</a>
+              <a href="#" class="col-xl-3 col-12 btnM">MANO DELFI</a>
             </div>
             </div>  
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="col headline">dhd</div>
+        <div class="col headline"></div>
+      </div>
+      <div class="row">
+        <div class="col content">
+          <div class="row">
+            <div class="col-12 col-xl-3 leftside">
+              <div class="row justify-content-sm-center">
+                <div class="col l1">
+                  <div class="row ">
+                    <div class="col  l2">Turinys</div>
+                  <div class="row ">
+                    <a href="#" class="col-12">Delfi naujienos</a>
+                    <a href="#" class="col-12">Verslas</a>
+                    <a href="#" class="col-12">Sportas</a>
+                    <a href="#" class="col-12">Pramogos</a>
+                    <a href="#" class="col-12">DELFI moterims</a>
+                    <a href="#" class="col-12">Archyvas</a>
+                    <a href="#" class="col-12">DELFI šiandien</a>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-xl-6">
+              <div class="row">
+                <div class="col search">
+                  <div class="row">
+                    <div class="col t">
+                      <div class="row">
+                        <a class="col" href="#">Žinios</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row op">
+                    <div class="col stxt"><input type="text" name="searchtxt"></div>
+                    <div class="col sel"><select name="searchsel">
+                      <option value="delfiweb">Delfi WebAZ</option>
+                      <option value="kas">Kas? Kur? Kada?</option>
+                    </select></div>
+                    <button type="button" class="col-xl-2 col-12 btnn">Ieškoti</button>
+                  </div>
+                </div>
+              </div>
+              <div class="row"><div class="col" style="color: blue; font-weight: bold;">Žinios</div></div>
+              <div class="row">
+                <div class="col aktualijos">
+                  <div class="row">
+                    <a href="#">Duotas startas rinkti parašus Seimo rinkimų sistemos keitimo referendumui</a>
+                    <p><img style="float: right; height: 150px; width: 150px;"  src="https://web.archive.org/web/20030220041239im_/http://g.delfi.lt/images/pix/file1895538_Uspaskichas.jpg_maz.JPG">Vyriausioji rinkimų komisija (VRK) trečiadienį išdavė parašų rinkimo lapus iniciatyvinei grupei, siekiančiai surengti referendumą dėl Seimo rinkimų sistemos keitimo. Išdavus šiuos lapus pradedamas skaičiuoti trijų mėnesių terminas, per kurį referendumo iniciatoriai turi surinkti ne mažiau kaip 300 tūkst. rinkimų teisę turinčių piliečių parašų.</p>
+                  </div>
+                  <div class="row">
+                    <a href="#">Hakeriai pavogė duomenis apie 8 mln. kredito kortelių</a>
+                    <p><img style="float: right; height: 100px; width: 150px;"  src="https://web.archive.org/web/20030220041239im_/http://g.delfi.lt/images/pix/file190998_visasmall.jpg">Kompiuterių įsilaužėliai įsibrovė į sandorius pagal kredito korteles vykdančių bendrovių "Visa" ir "MasterCard" apsaugos sistemą. JAV televizijos ir radijo bendrovės CNN duomenimis, nežinomi hakeriai gavo 8 milijonų JAV ir tarptautinių klientų kredito kortelių duomenis.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-xl-3">
+              <div class="row">
+                <div class="col">asfd</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
 
   </div>
+  <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+<!-- Use any element to open the sidenav -->
+
+
+<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+<div id="main">
+</div>
 </body>
 </html>
